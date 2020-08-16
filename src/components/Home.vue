f32f12986e22d2b000c696da6b58282271e19e8d
que2017 <1563645183@qq.com>
2020-08-16 11:33:15 +0800

    使用vant-ui的tab组件实现Home界面的标签切换

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <van-tabs v-model="active">
+		<van-tab title="华为">华为手机</van-tab>
+		<van-tab title="荣耀">荣耀手机</van-tab>
+		<van-tab title="小米">小米手机</van-tab>
+		<van-tab title="苹果">苹果手机</van-tab>
+		<van-tab title="OPPO">oppo手机</van-tab>
+		<van-tab title="VIVO">vivo手机</van-tab>
+	</van-tabs>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			active: 0
+		} 
+	},
+	methods: {
+		
+	},
+} 
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
